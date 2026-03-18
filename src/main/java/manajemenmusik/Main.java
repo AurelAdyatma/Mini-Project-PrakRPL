5af92bdff80d21b40d15409fe0f9e469d3aca691
AurelAdyatma <aureladyatma@gmail.com>
2026-03-18 21:36:23 +0700

    Manggil Login

--- src/main/java/manajemenmusik/Main.java
+++ src/main/java/manajemenmusik/Main.java
@@ -8,12 +8,25 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        MainView mainView = new MainView();
+        tampilkanLogin(primaryStage);
+    }
 
+    private void tampilkanLogin(Stage stage) {
+        LoginView loginView = new LoginView(() -> tampilkanDashboard(stage));
+        Scene scene = new Scene(loginView.getView(), 500, 420);
+
+        stage.setTitle("MiniProject Aplikasi Manajemen Musik");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    private void tampilkanDashboard(Stage stage) {
+        MainView mainView = new MainView();
         Scene scene = new Scene(mainView.getView(), 1100, 650);
-        primaryStage.setTitle("MiniProject Aplikasi Manajemen Musik");
-        primaryStage.setScene(scene);
-        primaryStage.show();
+
+        stage.setTitle("MiniProject Aplikasi Manajemen Musik");
+        stage.setScene(scene);
+        stage.show();
     }
 
     public static void main(String[] args) {
